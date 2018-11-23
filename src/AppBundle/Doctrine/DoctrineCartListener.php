74d552a28486d6c87c96bab8d32b43b80e9c0bef
Rochdy <le.roshdy@gmail.com>
2018-11-23 08:49:49 +0200

    using eventlistener

---
+++ src/AppBundle/Doctrine/DoctrineCartListener.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+use JMS\DiExtraBundle\Annotation\DoctrineListener as DoctrineDI;
+
+/**
+ * @DoctrineDI(
+ *     events = {"postPersist", "preRemove"},
+ *     connection = "default",
+ *     lazy = true,
+ *     priority = 0,
+ * )
+ */
+class DoctrineStoryListener
+{
+}
